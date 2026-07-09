e0906f486c1573076361c1e2402c4de6eb0c5e8d
Freya Aino <freya.aino@pm.me>
2026-07-09 17:54:56 +0200

    working inference on GPU

--- few-shot-classication/main.py
+++ few-shot-classication/main.py
@@ -11,24 +11,31 @@ import torch as T
 from torch import nn
 
 from functools import partial
+from torchvision.transforms import Compose, Resize, CenterCrop, Normalize, InterpolationMode, ToTensor
 
 # is the deployment environment dev (for debugging)
 # TODO - set by env var or something
 IS_DEV = False
-
-image_vector_collections = {
-    "A": [],
-    "B": [],
-    "C": [],
-    "D": []
+ENCODER_MODEL = "vit_base_patch16_dinov3.lvd1689m"
+MAX_NUM_VECTORS = 32
+INFERENCE_BUTTON = 8
+BUTTON_NAME_MAP = {
+    0: "A",
+    1: "B",
+    2: "C",
+    3: "D"
 }
+DEVICE = T.device("cuda") if T.cuda.is_available() else T.device("cpu")
+
+
+def put_save(loop, q, ell):
+    if loop.is_running():
+        asyncio.run_coroutine_threadsafe(q.put(ell), loop)
+
 
 def _init_model():
-
-    print("[_INIT_MODEL] initializing models")
-    
     model = timm.create_model(
-        'vit_base_patch16_dinov3.lvd1689m',
+        ENCODER_MODEL,
         pretrained=True,
         num_classes=0,
     ).eval()
@@ -37,17 +44,22 @@ def _init_model():
     # print("\n".join([f"{n} => {m}" for n, m in model.named_modules()]))
 
     # get model specific transforms (normalization, resize)
-    data_config = timm.data.resolve_model_data_config(model)
-    transforms = timm.data.create_transform(**data_config, is_training=False)
-
-    # output = model(transforms(img).unsqueeze(0))
+    # data_config = timm.data.resolve_model_data_config(model)
+    # transforms = timm.data.create_transform(**data_config, is_training=False)
+    transforms = T.jit.script(
+        T.nn.Sequential(
+            Resize(size=(256, 256), interpolation=InterpolationMode.BICUBIC, max_size=None, antialias=True),
+            CenterCrop(size=(256, 256)),
+            Normalize(mean=[0.4850, 0.4560, 0.4060], std=[0.2290, 0.2240, 0.2250])
+        )
+    )
     return model, transforms
 
-def _run_inference(model, transforms, frame):
-    # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    frame = Image.fromarray(frame)
-    user_input = transforms(frame).unsqueeze(0)
-    return model(user_input)
+def _infer_model(model, transforms, frame):
+    # features = await loop.run_in_executor(None, _run_inference, model, transform, frame)
+    frame = ToTensor()(frame).to(dtype=T.float32, device=DEVICE)
+    frame = transforms(frame).unsqueeze(0)
+    return model(frame)
 
 
 def input_thread(queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_event: asyncio.Event):
@@ -55,32 +67,42 @@ def input_thread(queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_eve
     pygame.init()
     joysticks = []
 
+    if pygame.joystick.get_count() <= 0:
+        print("[INPUT] WARNING - no controler detected")
+
     for i in range(0, pygame.joystick.get_count()):
         joysticks.append(pygame.joystick.Joystick(i))
         joysticks[-1].init()
 
-    def put(a):
-        if loop.is_running():
-            asyncio.run_coroutine_threadsafe(queue.put(a), loop)
-
-    print("[INPUT] started")
+    print("[INPUT] - started")
     try:
         while not stop_event.is_set():
             state = {}
+            
             for event in pygame.event.get():
-                state[event.type] = event.dict
+                
+                # print(event.type, pygame.JOYBUTTONDOWN, pygame.KEYDOWN)
+
+                if event.type == pygame.JOYBUTTONDOWN:
+                    state[event.type] = event.dict
+                # if event.type == pygame.KEYDOWN:
+                #     state[event.type] = event.dict
 
             if state != {}:
+                
+                print(f"[INPUT] recorded: {state}")
+
                 if queue.full():
                     try:
                         queue.get_nowait()
                     except asyncio.QueueEmpty:
                         pass
-                put(state)
+                put_save(loop, queue, state)
              
     except Exception as e:
-        print(f"[INPUT] error: {e}")
+        print(f"[INPUT] ERROR - {e}")
     finally:
+        stop_event.set()
         print("[INPUT] stopped")
 
 
@@ -88,10 +110,6 @@ def camera_thread(queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_ev
 
     webcam = Webcam(src=0, w=640)
     print("[CAMERA] started")
-
-    def put(frame):
-        if loop.is_running():
-            asyncio.run_coroutine_threadsafe(queue.put(frame), loop)
 
     try:
         for frame in webcam:
@@ -103,13 +121,13 @@ def camera_thread(queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_ev
                     queue.get_nowait()
                 except asyncio.QueueEmpty:
                     pass
-            put(frame)
+            put_save(loop, queue, frame)
     except Exception as e:
         print(f"[CAMERA] error: {e}")
     finally:
         webcam.release()
+        stop_event.set()
         print("[CAMERA] stopped")
-
 
 async def camera_task(queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_event: asyncio.Event):
     await loop.run_in_executor(None, camera_thread, queue, loop, stop_event)
@@ -117,37 +135,11 @@ async def camera_task(queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, sto
 async def input_task(queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_event: asyncio.Event):
     await loop.run_in_executor(None, input_thread, queue, loop, stop_event)
 
-async def inference_task(frame_queue: asyncio.Queue, input_queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_event: asyncio.Event):
+async def process_task(frame_queue: asyncio.Queue, input_queue: asyncio.Queue, result_queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_event: asyncio.Event):
 
-    button_vectors = {
-        0: [],
-        1: [],
-        2: [],
-        3: []
-    }
-    def handle_features(future, button_input):
-        try:
-            
-            features = future.result()
-            button_vectors[button_input["button"]].append(features)
-
-            print(f"[inference] shape={features.shape} mean={features.mean():.4f}")
-            print("\n".join([f"{k}: {len(e)}" for k, e in button_vectors.items()]))
-
-        except Exception as e:
-            print(f"[inference] error - {e}")
-
-
+    print("[PROCESS] start")
     try:
-        model, transform = await loop.run_in_executor(None, _init_model)
-    except Exception as e:
-        print("[INFERENCE] failed : model initalization")
-        stop_event.set()
-        return
-
-    print("[INFERENCE] start")
-    try:
-        while True:
+        while not stop_event.is_set():
             frame = await frame_queue.get()
 
             cv2.imshow("webcam frames", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
@@ -163,40 +155,97 @@ async def inference_task(frame_queue: asyncio.Queue, input_queue: asyncio.Queue,
             if button_input is None:
                 continue
                 
-            if button_input["button"] not in button_vectors.keys():
-                continue
-
-            infer = partial(_run_inference, model, transform, frame)
-            try:
-                future = loop.run_in_executor(None, infer)
-                future.add_done_callback(partial(handle_features, button_input=button_input))
-            except Exception as e:
-                print(f"[inference] failed - frame: {e}")
+            if result_queue.full():
+                # put_save(loop, result_queue, (frame, button_input))
+                try:
+                    result_queue.get_nowait()
+                except asyncio.QueueEmpty:
+                    pass
+            put_save(loop, result_queue, (frame, button_input))
 
     except Exception as e:
-        print(f"[INFERENCE] failed - main loop: {e}")
+        print(f"[PROCESS] failed - main loop: {e}")
     finally:
         cv2.destroyAllWindows()
         stop_event.set()
-        print("[INFERENCE] stopped")
+        print("[PROCESS] stopped")
+
+
+async def training_task(result_queue: asyncio.Queue, loop: asyncio.AbstractEventLoop, stop_event: asyncio.Event):
+
+    try:
+        print("[INFERENCE]: initializing model")
+        model, transforms = await loop.run_in_executor(None, _init_model)
+        model = model.to(device=DEVICE)
+        transforms = transforms.to(device=DEVICE)
+    except Exception as e:
+        print(f"[INFERENCE] failed - model initialization: {e}")
+        stop_event.set()
+        return
+
+    button_vectors = {}
+    obj_tensors = {}
+    ojb_tensors_computed = False
+
+    try:
+        print("[INFERENCE] - starting")
+        while not stop_event.is_set():
+
+            frame, button_input = await result_queue.get()
+            
+            print(f"[inference] processing frame for button {button_input}")
+
+            features = await loop.run_in_executor(None, _infer_model, model, transforms, frame)
+        
+            # switch between training and inference
+            if button_input["button"] == INFERENCE_BUTTON:
+                # compute tensors
+                print("[inference] stacking tensors")
+                if not ojb_tensors_computed:
+                    for k, v in button_vectors.items():
+                        obj_tensors[k] = T.stack(v, dim=1).to(device=DEVICE)
+                    ojb_tensors_computed = True
+
+                distances = {BUTTON_NAME_MAP[k]: T.dist(obj_tensors[k].mean(dim=0).squeeze(), features.squeeze()) for k in obj_tensors}
+                print(distances)
+                continue
+
+            if button_input["button"] in button_vectors:
+                button_vectors[button_input["button"]].append(features.to("cpu"))
+            else:
+                button_vectors[button_input["button"]] = [features.to("cpu")]
+            ojb_tensors_computed = False
+
+            # print(f"[inference] shape={features.shape} mean={features.mean():.4f}")
+            print("\n".join([f"{BUTTON_NAME_MAP[k]}: {len(e)}" for k, e in button_vectors.items()]))
+
+
+    except Exception as e:
+        print(f"[INFERENCE] ERROR - {e}")
+    finally:
+        stop_event.set()
+        print("[INFERENCE] - stopped")
+
 
 
 async def amain(loop: asyncio.AbstractEventLoop):
     
     frame_queue = asyncio.Queue(maxsize=2)
     input_queue = asyncio.Queue(maxsize=2)
+    result_queue = asyncio.Queue(maxsize=10)
     stop_event = asyncio.Event()
     
     result = await asyncio.gather(
-        asyncio.create_task(camera_task(frame_queue, loop, stop_event)), 
-        asyncio.create_task(inference_task(frame_queue, input_queue, loop, stop_event)),
         asyncio.create_task(input_task(input_queue, loop, stop_event)),
+        asyncio.create_task(camera_task(frame_queue, loop, stop_event)), 
+        asyncio.create_task(process_task(frame_queue, input_queue, result_queue, loop, stop_event)),
+        asyncio.create_task(training_task(result_queue, loop, stop_event)),
         return_exceptions=True
     )
     
     for r in result:
         if isinstance(r, Exception):
-            print(f"[amain] failed - task raised {r}")
+            print(f"[AMAIN] ERROR - task raised {r}")
 
 def main():
     loop = asyncio.new_event_loop()
@@ -204,12 +253,14 @@ def main():
 
     try:
         loop.run_until_complete(amain(loop))
+    except Exception as e:
+        print(e.with_traceback)
     finally:
-        print("[MAIN] trying to shut down ")
+        print("[MAIN] - trying to shut down ")
         loop.run_until_complete(loop.shutdown_asyncgens())
         loop.run_until_complete(asyncio.sleep(1.0))
         loop.close()
-        print("[MAIN] loop closed")
+        print("[MAIN] - loop closed")
 
 if __name__ == "__main__":
     main()
